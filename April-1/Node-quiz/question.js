cd23165ba807606826ef5a4f57915e486c31a795
Madhuvanthi Srikanth <madhuvanthisrikanth21820@gmail.com>
2022-04-11 10:10:48 +0530

    quiz

---
+++ April-1/Node-quiz/question.js
@@ -0,0 +1,22 @@
+module.exports.data = [
+    {
+        id:1,
+        question:"Question - 1",
+        options:["option - 1",
+                 "option - 2",
+                 "option - 3",
+                 "option-4"],
+        answer:'option - 4',
+    },
+    {
+        id:2,
+        question:"Question - 2",
+        options:["option - 1",
+                 "option - 2",
+                 "option - 3",
+                 "option-4"],
+        
+        answer:'option - 2',
+    },
+    
+]
