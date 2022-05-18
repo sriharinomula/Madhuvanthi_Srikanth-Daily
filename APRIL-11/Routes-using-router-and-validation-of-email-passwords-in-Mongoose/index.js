9a373d209435761687a8558ecde51b09f4a8df38
Madhuvanthi Srikanth <madhuvanthisrikanth21820@gmail.com>
2022-05-18 11:08:46 +0530

    Create index.js

---
+++ APRIL-11/Routes-using-router-and-validation-of-email-passwords-in-Mongoose/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const {initDB} = require('./models');
+const {userRouter} = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(initDB);
+
+app.use('/users',userRouter);
+
+app.listen(3000,()=>console.log('Listening...!!!'));
+
+
+
